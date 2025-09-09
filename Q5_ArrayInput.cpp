80429e63396d7b4953a4f546a12579fc16a6cc33
anuragji111 <anandjha1209@gmail.com>
2025-09-10 02:42:55 +0530

    DSA: Added Q5 Array Input and Display program with explanation

--- Q5_ArrayInput.cpp
+++ Q5_ArrayInput.cpp
@@ -1,19 +1,29 @@
-//Q.5:-
+// Q.5: Program to Input N Elements in an Array and Display Them
+// Explanation:
+// 1. User enters size of array (n).
+// 2. User inputs n elements.
+// 3. Program prints entered elements.
+// Learning: Basics of arrays & iteration in C++.
 
-
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
 int main() {
-   int n;
-   cout << "Enter the Number of Elements you want";
-   cin >> n;
-   int arr[100];
-   cout<<"Enter the Elements of the Array";
-   for(int i=0; i<n; i++){
-    cin>>arr[i];
-   }
-   cout<<"you have Entered the elements";
-   
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "You have entered: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
